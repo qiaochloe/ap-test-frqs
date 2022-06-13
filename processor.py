cac77260e2d47ce1e23ef06728769aaca54045b0
Chloe Qiao <qiaochloe@gmail.com>
2022-06-12 20:04:02 -0700

    Create fill_in_nan and main function

--- processor.py
+++ processor.py
@@ -1,14 +1,9 @@
 import re
 import pandas as pd
 import numpy as np
-from preprocessing import preprocess_file_content 
-
-EARLIEST_YEAR = 1954
-LATEST_YEAR = 2022
-
-file_name = "ap21-frq-world-history.txt"
-file_regex = "^([0-9]\.\s)(.*?)((?=\n[0-9]\.)|(?=\s\s\s)|(?=\nDocument [0-9]\s))$"
-question_keys = ["SAQ1", "SAQ2", "SAQ3", "SAQ4", "DBQ1", "LEQ2", "LEQ3", "LEQ4"]
+from datetime import date
+from preprocessor import preprocess_file_content 
+from os import listdir
 
 def get_file_content(file_name):
     """
@@ -24,41 +19,53 @@ def get_file_content(file_name):
         
     return file_content
 
+EARLIEST_YEAR = 1954
+
+def get_latest_year():
+    if date.today().month >  6:
+        latest_year = date.today().year
+    else: 
+        latest_year = date.today().year - 1
+    return latest_year
+
+LATEST_YEAR = get_latest_year()
+    
 def get_year(file_content, file_name):
+
     """
     Args: 
         file_content (str): content of the text file
         file_name (str): name of the text file
     
     Returns:
-        int: year of the exam; NaN if the year is not found
+        str: year of the exam; NaN if the year is not found
     """
     # search content of the file
     year_regex = "\d+"
     for match in re.finditer(year_regex, file_content):
         value = match.group(0)
         if len(value) == 4 and EARLIEST_YEAR <= int(value) <= LATEST_YEAR:
-            return int(value)
+            return value
     
     # search the name of the file
     for match in re.finditer("\d+", file_name): # all numbers in a str
         value = match.group(0)
         if len(value) == 4 and EARLIEST_YEAR <= int(value) <= LATEST_YEAR:
-            return int(value)
+            return value
         elif len(value) == 2:
             if int(value) <= int(str(LATEST_YEAR)[-2:]):
-                return int("20" + value)
+                return "20" + value
             elif int(value) >= int(str(EARLIEST_YEAR)[-2:]):
-                return int("19" + value)
+                return "19" + value
             
     return np.NaN
 
-def get_questions(file_content, file_regex):
+def get_questions(file_content, question_regex):
     """Gets a list of questions
     
     Args: 
         file_content (str): content of the text file
-        file_regex (str): regex to parse file
+        question_regex (str): regex to parse file
     
     Returns:
         list: list of exam questions in chronological order (as they appear in the text)
@@ -67,7 +74,7 @@ def get_questions(file_content, file_regex):
     questions = []
     count = 0
     
-    for match in re.finditer(file_regex, file_content, flags=re.I|re.M|re.S):
+    for match in re.finditer(question_regex, file_content, flags=re.I|re.M|re.S):
         
         question = match.group(2) # string
         
@@ -90,21 +97,27 @@ def get_questions(file_content, file_regex):
 def get_question_type(year):
     """
     Args:
-        year (int): year of the exam
+        year (str): year of the exam
     
     Returns:
         question_type (list): type of each FRQ (SAQ, DBQ, LEQ)
         question_number (list): question number of each FRQ as given in the text
     """
     
-    if np.isnan(year): 
-        return [np.nan for i in range(10)], [np.nan for i in range(10)]
+    if pd.isna(year): 
+        return [np.nan], [np.nan]
     
-    elif year >= 2017:
+    elif int(year) > 2017:
         questions = [["SAQ", "1"], ["SAQ", "2"],
                     ["SAQ", "3"], ["SAQ", "4"],
                     ["DBQ", "1"], ["LEQ", "2"],
                     ["LEQ", "3"], ["LEQ", "4"]]
+        
+    elif int(year) == 2017:
+        questions = [["SAQ", "1"], ["SAQ", "2"],
+                    ["SAQ", "3"], ["SAQ", "4"],
+                    ["DBQ", "1"], ["LEQ", "2"],
+                    ["LEQ", "3"]]
     
     else:
         questions = [["DBQ", "1"], ["LEQ", "2"], ["LEQ", "3"]]
@@ -114,28 +127,50 @@ def get_question_type(year):
     
     return question_type, question_number
 
-def get_df(year, questions, question_type, question_number):
+def fill_in_nan(questions, question_type, question_number):
+            
+    series = [pd.Series(lst, dtype=str) for lst in [questions, question_type, question_number]]
+    df = pd.concat(series, axis=1) # fills in NaN
+    
+    questions = df.iloc[:,0].tolist()
+    question_type = df.iloc[:,1].tolist()
+    question_number = df.iloc[:,2].tolist()
+    
+    return questions, question_type, question_number
+    
+def get_df(file_name, question_regex):
     """
     Args:
-        year (int): year of exam
-        questions (list): list of FRQs in order
-        question_type (list): list of the type of each FRQ 
-        question_number (list): list of the question number of each FRQ 
+        file_name: name of the exam
+        question_regex: regex to parse exam questions
         
     Returns:
         pd.DataFrame: df of the FRQs for a given year
     """
     
+    file_content = preprocess_file_content(get_file_content(file_name))
+    
+    year = get_year(file_content, file_name)    
+    questions = get_questions(file_content, question_regex)
+    question_type, question_number = get_question_type(year)
+    questions, question_type, question_number = fill_in_nan(questions, question_type, question_number)
+    
     columns = ["question", "question_type", "question_number", "year"]
     df = pd.DataFrame(columns=columns)
+    
+    #len(questions), len(question_type), len(question_number)
     
     count = 0
     while count < len(questions):
         df.loc[count + 1] = [questions[count], question_type[count], question_number[count], year]
         count += 1
-
+    
     return df
     
+def get_files(dir_name):
+    files = [file for file in listdir(dir_name) if file.endswith(".txt")]
+    return files
+
 def create_csv(df, file_name):
     """
     Args:
@@ -144,22 +179,15 @@ def create_csv(df, file_name):
     """
     
     with open(f"{file_name}", "w+") as file:
-        df.to_csv(file)
+        df.to_csv(file, index=False)
+
+def main(exam, question_regex):
+    files = get_files(f"{exam}/pdf-text")
     
-def main(data_file_name, file_regex, output_file_name):
+    file_dfs = [get_df(f"{exam}/pdf-text/{file}", question_regex) for file in files]
+    output_df = pd.concat(file_dfs, ignore_index=True, sort=False)
+    create_csv(output_df, f"{exam}/data.csv")
     
-    file_content = preprocess_file_content(get_file_content(data_file_name))
+main("ap-world-history", "^([0-9]\.\s)(.*?)((?=\n[0-9]\.)|(?=\s\s\s)|(?=\nDocument [0-9]\s))$")
     
-    year = get_year(file_content, data_file_name)    
-    questions = get_questions(file_content, file_regex)
-    question_type, question_number = get_question_type(year)
-    
-    df = get_df(year, questions, question_type, question_number)
-    create_csv(df, output_file_name)
-    
-main("./test-data/ap19-frq-world-history.txt", file_regex, "test.csv")
-                    
-# ^([0-9]\.\s)(.*?)((?=\n[0-9]\.)|(?=\s\s\s)|(?=\nDocument [0-9]\s))$
-# re.M
-# re.S
-# re.I
+#main("./test-data/ap19-frq-world-history.txt", question_regex, "test.csv")
