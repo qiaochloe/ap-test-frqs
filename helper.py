59d3c2a3356951697edf4a36b62ffdcfb34cb7bf
Chloe Qiao <qiaochloe@gmail.com>
2022-06-16 20:47:57 -0700

    Remove var-len *regex_list from remove_phrases

--- helper.py
+++ helper.py
@@ -11,23 +11,22 @@ def get_file_content(file_name):
     
     with open(file_name, encoding="utf-8") as file:
         file_content = file.read() # string
-        
     return file_content
 
-def remove_phrases(file_content, *regex_lists, regex_flags=re.I|re.M):
+def remove_phrases(file_content, regex_list, regex_flags=re.I|re.M):
     """Removes phrases using regex
     
     Args:
         file_content (str): text content 
-        regex_lists (list): list of regex to remove phrases by
+        regex_list (list): list of regex to remove phrases by
         regex_flags
         
     Returns:
         str: the text content with phrases removed 
     """
-        
-    for regex_list in regex_lists:
-        for regex in regex_list: 
-            file_content = re.sub(regex, '', file_content, flags=regex_flags)
     
+    if type(regex_list) is not list:
+        regex_list = [regex_list]
+    for regex in regex_list: 
+        file_content = re.sub(regex, '', file_content, flags=regex_flags)
     return file_content
