7eb9e7f601829ff3d098fe0832567669b0f906f0
Chloe Qiao <qiaochloe@gmail.com>
2022-06-29 17:43:29 -0700

    Change the postprocessing script to a class method

--- processor.py
+++ processor.py
@@ -2,6 +2,12 @@
 # Clean the sources.csv
 # Separate APWH-specific parts from the functions (make them reuseable)
 
+# TODO:
+# get link to scoring guidelines PDF
+# add "Source:" as a header for a document; remove before getting questions
+# add section for "Historical Background:"; also remove before getting questions
+# consider serving regex as tuples (for the flags)
+
 import re
 import pandas as pd
 import numpy as np
@@ -9,11 +15,11 @@ from datetime import date
 from preprocessor import preprocess_file_content
 from os import listdir
 
-        
+
 class Exam:
     earliest_year = 1954
     latest_year = date.today().year if date.today().month > 6 else date.today().year - 1
-    
+
     @classmethod
     def get_year(cls, file_name, file_content):
         """
@@ -32,7 +38,7 @@ class Exam:
                 return value
 
         # search the name of the file
-        for match in re.finditer("\d+", file_name): # all numbers in a str
+        for match in re.finditer("\d+", file_name):  # all numbers in a str
             value = match.group(0)
             if len(value) == 4 and EARLIEST_YEAR <= int(value) <= LATEST_YEAR:
                 return value
@@ -43,32 +49,35 @@ class Exam:
                     return "19" + value
 
         return np.NaN
-    
+
     @staticmethod
-    def remove_phrases(file_content, regex_list, regex_flags=re.I|re.M):
+    def remove_phrases(file_content, regex_list, regex_flags=re.I | re.M):
         """Removes phrases using regex
-        
+
         Args:
-            file_content (str): text content 
+            file_content (str): text content
             regex_list (list): list of regex to remove phrases by
             regex_flags
-            
+
         Returns:
-            str: the text content with phrases removed 
+            str: the text content with phrases removed
         """
-        
+
         if type(regex_list) is not list:
             regex_list = [regex_list]
-        for regex in regex_list: 
-            file_content = re.sub(regex, '', file_content, flags=regex_flags)
+        for regex in regex_list:
+            file_content = re.sub(regex, "", file_content, flags=regex_flags)
         return file_content
+
 
 class WorldHistoryExam(Exam):
     name = "ap-world-history"
     doc_regex = r"^(Document\s*)(\d)(.*?)(?=(Document|END))"
     source_regex = r"^(Use the (.*?) to answer all parts of the question that follows\.\s*)(.*?)((?<!\d)(?=([1-9])\.))"
-    question_regex = r"^([0-9]\.)(.*?)((?=\n[1-9]\.)|(?=\s\s\s)|(?=\nDocument [0-9]\s)|(?=\sEND))$"
-    
+    question_regex = (
+        r"^([0-9]\.)(.*?)((?=\n[1-9]\.)|(?=\s\s\s)|(?=\nDocument [0-9]\s)|(?=\sEND))$"
+    )
+
     @classmethod
     def get_documents(cls, file_name, year, file_content):
         """
@@ -80,7 +89,7 @@ class WorldHistoryExam(Exam):
         Returns:
             list: list of all documents (in string form)
         """
-            
+
         docs = []
 
         for match in re.finditer(cls.doc_regex, file_content, flags=re.M | re.S):
@@ -88,10 +97,19 @@ class WorldHistoryExam(Exam):
             doc_content = match.group(3)
             question_type = "DBQ"
             question_number = "1"
-            docs.append([doc_content, doc_number, question_type, question_number, year, file_name])
+            docs.append(
+                [
+                    doc_content,
+                    doc_number,
+                    question_type,
+                    question_number,
+                    year,
+                    file_name,
+                ]
+            )
 
         return docs
-    
+
     @classmethod
     def get_sources(cls, file_name, year, file_content):
         """
@@ -113,7 +131,14 @@ class WorldHistoryExam(Exam):
             question_type = "SAQ"
             question_number = match.group(5)
             sources.append(
-                [source_content, source_number, question_type, question_number, year, file_name]
+                [
+                    source_content,
+                    source_number,
+                    question_type,
+                    question_number,
+                    year,
+                    file_name,
+                ]
             )
 
         return sources
@@ -136,7 +161,7 @@ class WorldHistoryExam(Exam):
             "question_type",
             "question_number",
             "year",
-            "file_name"
+            "file_name",
         ]
 
         df = pd.DataFrame(
@@ -158,7 +183,9 @@ class WorldHistoryExam(Exam):
             str: file content without the documents or sources
         """
 
-        file_content = cls.remove_phrases(file_content, [cls.doc_regex], regex_flags=re.M | re.S)
+        file_content = cls.remove_phrases(
+            file_content, [cls.doc_regex], regex_flags=re.M | re.S
+        )
         file_content = cls.remove_phrases(
             file_content, [cls.source_regex], regex_flags=re.M | re.S
         )
@@ -191,7 +218,7 @@ class WorldHistoryExam(Exam):
             saq_range = range(0)
             dbq_range = range(1, 2)
             leq_range = range(2, 4)
-        
+
         questions = (
             [["SAQ", str(i)] for i in saq_range]
             + [["DBQ", str(i)] for i in dbq_range]
@@ -200,7 +227,7 @@ class WorldHistoryExam(Exam):
         question_type = [question[0] for question in questions]
         question_number = [question[1] for question in questions]
         return question_type, question_number
-    
+
     @classmethod
     def get_questions(cls, file_content):
         """Gets a list of questions
@@ -215,8 +242,14 @@ class WorldHistoryExam(Exam):
         questions = []
         count = 0
 
-        for match in re.finditer(cls.question_regex, file_content, flags=re.I | re.M | re.S):
+        for match in re.finditer(
+            cls.question_regex, file_content, flags=re.I | re.M | re.S
+        ):
             question = match.group(2)  # string
+
+            # Filter out matches that can't be questions (hacky)
+            if len(question) < 20:
+                continue
 
             # remove a) b) c) and convert to subquestions list
             question = re.sub("^[abc]\)\s", "", question, flags=re.I | re.M | re.S)
@@ -239,7 +272,7 @@ class WorldHistoryExam(Exam):
             count += 1
 
         return questions
-    
+
     @staticmethod
     def fill_in_nan(questions, question_type, question_number):
         """
@@ -254,7 +287,8 @@ class WorldHistoryExam(Exam):
         """
 
         series = [
-            pd.Series(lst, dtype=str) for lst in [questions, question_type, question_number]
+            pd.Series(lst, dtype=str)
+            for lst in [questions, question_type, question_number]
         ]
         df = pd.concat(series, axis=1)  # fills in NaN
 
@@ -263,7 +297,7 @@ class WorldHistoryExam(Exam):
         question_number = df.iloc[:, 2].tolist()
 
         return questions, question_type, question_number
-    
+
     @classmethod
     def get_question_df(cls, file_name, year, file_content):
         """
@@ -292,18 +326,36 @@ class WorldHistoryExam(Exam):
                 question_type[count],
                 question_number[count],
                 year,
-                file_name
+                file_name,
             ]
             count += 1
 
         return df
+
+    @staticmethod
+    def clean_questions(df):
+        def strip_chars(question):
+            try:
+                question = question.strip().replace("  ", " ")
+                question = re.sub("^[\dabc](\)|\.)", "", question, flags=re.I | re.M)
+            finally:
+                return question
+
+        df["question"] = df["question"].apply(strip_chars)
+        return df
+
+    @classmethod
+    def postprocessor(cls, question_df, source_df):
+        question_df = cls.clean_questions(question_df)
+        return question_df, source_df
+
 
 class EuropeanHistoryExam(WorldHistoryExam):
     name = "ap-european-history"
 
     @staticmethod
     def get_question_type(year):
-        
+
         if pd.isna(year):
             return [np.nan], [np.nan]
 
@@ -319,15 +371,45 @@ class EuropeanHistoryExam(WorldHistoryExam):
             saq_range = range(0)
             dbq_range = range(1, 2)
             leq_range = range(2, 8)
-    
+
         questions = (
-                [["SAQ", str(i)] for i in saq_range]
-                + [["DBQ", str(i)] for i in dbq_range]
-                + [["LEQ", str(i)] for i in leq_range]
-            )
+            [["SAQ", str(i)] for i in saq_range]
+            + [["DBQ", str(i)] for i in dbq_range]
+            + [["LEQ", str(i)] for i in leq_range]
+        )
         question_type = [question[0] for question in questions]
         question_number = [question[1] for question in questions]
         return question_type, question_number
+
+    @staticmethod
+    def patch_one(question_df):
+        # question_regex doesn't recognize semicolons (such as "Question 1:")
+        # This patches the data for the 1999 txt file
+
+        index = question_df[question_df["year"] == "1999"].index[0]
+        questions = [
+            "For the period 1861 to 1914, analyze how various Russians perceived the condition of the Russian peasantry and explain how they proposed to change that condition.",
+            "Contrast how a Marxist and a Social Darwinist would account for the differences in the conditions of these two mid-nineteenth-century families.",
+            "Analyze the ways in which the contrasting styles of these two paintings reflect the different economic values and social structures of France and the Netherlands in the seventeenth century.",
+            "Contrast the historical context, beliefs, and behavior of quepean youth represented by these two photographs.",
+            """Machiavelli suggested that a ruler should behave both "like a lion" and "like a fox." Analyze the policies of TWO of the following quepean rulers, indicating the degree to which they successfully followed Machiavellis suggestion.
+        Choose two: Elizabeth I of England
+        Henry IV of France
+        Catherine the Great of Russia
+        Frederick II of Prussia""",
+            """Discuss the relationship between politics and religion by examining the wars of religion. Choose TWO specific examples from the following:
+        Dutch Revolt  French Wars of Religion  English Civil War Thirty Years' War""",
+            "Compare and contrast the degree of success of treaties negotiated in Vienna (1814-1815) and Versailles (1919) in achieving quepean stability.",
+        ]
+
+        question_df.loc[index : index + 6, "question"] = questions
+        return question_df
+
+    @classmethod
+    def postprocessor(cls, question_df, source_df):
+        question_df = cls.clean_questions(question_df)
+        question_df = cls.patch_one(question_df)
+        return question_df, source_df
 
 
 def get_files(dir_name):
@@ -346,15 +428,15 @@ def get_files(dir_name):
 
 def get_file_content(file_name):
     """
-    Args: 
+    Args:
         file_name (str): name of the text file
-    
-    Returns: 
+
+    Returns:
         str: content of the text file
     """
-    
+
     with open(file_name, encoding="utf-8") as file:
-        file_content = file.read() # string
+        file_content = file.read()  # string
     return file_content
 
 
@@ -369,18 +451,6 @@ def create_csv(df, file_name):
         df.to_csv(file, index=False)
 
 
-def clean_csv_questions(df):
-    def clean_question(question):
-        try:
-            question = question.strip().replace("  ", " ")
-            question = re.sub("^[\dabc](\)|\.)", "", question, flags=re.I|re.M)
-        finally:
-            return question
-    
-    df["question"] = df["question"].apply(clean_question)
-    return df
-
-
 def main(exam):
     files = get_files(f"{exam.name}/pdf-text")
 
@@ -390,7 +460,7 @@ def main(exam):
     for file in files:
         file_content = get_file_content(f"{exam.name}/pdf-text/{file}")
         year = exam.get_year(file, file_content)
-        
+
         file_content = preprocess_file_content(file_content)
         source_dfs_list.append(exam.get_source_df(file, year, file_content))
 
@@ -399,8 +469,9 @@ def main(exam):
 
     source_dfs = pd.concat(source_dfs_list, ignore_index=True, sort=False)
     question_dfs = pd.concat(question_dfs_list, ignore_index=True, sort=False)
-    question_dfs = clean_csv_questions(question_dfs)
-    
+
+    question_dfs, source_dfs = exam.postprocessor(question_dfs, source_dfs)
+
     create_csv(source_dfs, f"{exam.name}/source.csv")
     create_csv(question_dfs, f"{exam.name}/question.csv")
 
@@ -410,8 +481,8 @@ def main(exam):
 euro = EuropeanHistoryExam()
 main(euro)
 
-#apwh = WorldHistoryExam()
-#main(apwh)
+# apwh = WorldHistoryExam()
+# main(apwh)
 
 # with open("test.txt", "w+") as file:
 #    content = remove_sources(preprocess_file_content(get_file_content("ap-world-history/pdf-text/ap-world-history-frq-2017.txt")))
